05fb7484125ba865a8f68eabeec8faa8cc9ce7a5
Lahiruhiran <38062304+Lahiruhiran@users.noreply.github.com>
2020-02-08 22:27:59 +0530

    First commit of the page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bloom</title>
+    <link rel ="stylesheet" type ="text/css" href ="assest/css/style.css">
+</head>
+<body>
+    <div class ="warpper IndexPage">
+    <div class="mainSection">
+        <div class="logContainer">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+        </div>
+        <div class="searchContainer">
+            <form action="search.php" Method ="GET">
+            <input class="searchBox" type="text" name="term">
+            <input class="searchButton" type="submit" value="Search">
+            </form>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
