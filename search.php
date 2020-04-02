0971aa52071aa233310eb65dda3967882e7e70f5
unknown <lahiruhiranwijesooriyansbm@gmail.com>
2020-04-02 22:06:30 +0530

    create search.php

--- search.php
+++ search.php
@@ -1,4 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bloom</title>
+    <link rel ="stylesheet" type ="text/css" href ="assest/css/style.css">
+</head>
+<body>
+    <div class ="warpper IndexPage">
+        <div class="header">
 
-echo "hello Lara";
-?>
+            <div class="headerContent">
+
+                <div class="logContainer">
+                <a href ="index.php">
+                    <img src="assest/images/bloomee.png">
+                <a>
+                </div>
+            
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+                    
+                        <div class="searchBarContainer">
+                        
+                            <input class="searchBox" type="text" name="term">
+                        </div>
+                    
+                    </form>
+                
+                </div>
+            </div>
+        
+        </div>
+    </div>
+</body>
+</html>
